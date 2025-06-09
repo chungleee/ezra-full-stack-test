eef6116a73ef611043a2580d19e2c1213e8b74c2
Leon <leon.karho.chung@gmail.com>
2025-06-09 14:43:48 -0400

    Create New Task
    - create new task with input name validation
    - returns error if empty
    - create new db task row if valid
    - return all tasks endpoint

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,10 +1,42 @@
 import { Hono } from "hono";
+import { validator } from "hono/validator";
+import { prisma } from "../database/prisma.js";
 
 const tasks = new Hono();
 
-tasks.get("/", (c) => {
+tasks.post(
+	"/",
+	validator("json", (value, c) => {
+		const name = value["name"];
+		if (!name.length) {
+			return c.json({
+				type: "error",
+				message: "Name field is empty",
+			});
+		}
+		return value;
+	}),
+	async (c) => {
+		const { name } = c.req.valid("json");
+
+		const newTask = await prisma.task.create({
+			data: {
+				name,
+			},
+		});
+
+		return c.json({
+			type: "success",
+			data: newTask,
+		});
+	}
+);
+
+tasks.get("/", async (c) => {
+	const tasks = await prisma.task.findMany();
+
 	return c.json({
-		tasks: "/",
+		tasks,
 	});
 });
 
